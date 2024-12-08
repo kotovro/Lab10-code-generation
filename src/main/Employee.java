676567e4532085f351a82e61e179d9b1122ded4c
Kotvoro <kotvoro@gmail.com>
2024-12-08 07:30:14 +0300

    Initiqal commit

---
+++ src/main/Employee.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Employee {
+
+	public int SSnumber;
+	public String name;
+	public String email;
+	public int number_employee;
+
+	public void performanceOfOfficialDuties() {
+		// TODO - implement Employee.performanceOfOfficialDuties
+		throw new UnsupportedOperationException();
+	}
+
+	public void getSalary() {
+		// TODO - implement Employee.getSalary
+		throw new UnsupportedOperationException();
+	}
+
+}
